126d45ee7855db6b72e985a83e2dcad48023e9eb
Max Manthey <mantheymax23@gmail.com>
2022-08-23 00:39:34 +0200

    extracted df to class

--- src/main/scala/RssNewsEvaluation.scala
+++ src/main/scala/RssNewsEvaluation.scala
@@ -7,25 +7,10 @@ object RssNewsEvaluation {
   val connectionProperties = new Properties()
   connectionProperties.put("user", "sa")
   connectionProperties.put("password", "")
-  private val frequencyPerSourceQuery = "SELECT NW.word, WF.frequency, SD.DATE, SD.SOURCE " +
-    "FROM WORD_FREQUENCY WF " +
-    "JOIN NEWS_WORD NW on WF.NEWS_WORD_ID = NW.ID " +
-    "JOIN SOURCE_DATE SD on WF.SOURCE_DATE_ID = SD.ID " +
-    "WHERE NW.word = 'ukraine' " +
-    "AND SD.DATE BETWEEN '2022-05-20' AND '2022-08-21' " +
-    "ORDER BY SD.DATE;"
-  private val frequencyPerDayQuery = "SELECT NW.word, SUM(WF.frequency), SD.DATE " +
-    "FROM WORD_FREQUENCY WF " +
-    "JOIN NEWS_WORD NW on WF.NEWS_WORD_ID = NW.ID " +
-    "JOIN SOURCE_DATE SD on WF.SOURCE_DATE_ID = SD.ID " +
-    "WHERE NW.word = 'ukraine' " +
-    "AND SD.DATE BETWEEN '2022-05-20' AND '2022-08-21' " +
-    "GROUP BY SD.DATE, NW.word " +
-    "ORDER BY SD.DATE;"
 
 
   def main(args: Array[String]): Unit = {
-    if(args.length != 1) {
+    if(args.length != 4) {
       println("Amount of provided args incorrect.")
       sys.exit(1)
     }
@@ -37,22 +22,34 @@ object RssNewsEvaluation {
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val connectionUrl = "jdbc:h2:" + args(0) + "/rss_news_articles"
-    val sourceDateDF = spark.read.jdbc(connectionUrl, "source_date", connectionProperties)
-    sourceDateDF.createOrReplaceTempView("source_date")
-    val newsWordDF = spark.read.jdbc(connectionUrl, "news_word", connectionProperties)
-    newsWordDF.createOrReplaceTempView("news_word")
-    val wordFrequencyDF = spark.read.jdbc(connectionUrl, "word_frequency", connectionProperties)
-    wordFrequencyDF.createOrReplaceTempView("word_frequency")
+    val jdbcConnection = JdbcConnection(args(0), connectionProperties)
+    jdbcConnection.createTableTempView("source_date", spark)
+    jdbcConnection.createTableTempView("news_word", spark)
+    jdbcConnection.createTableTempView("word_frequency", spark)
 
+    val frequencyPerSourceQuery = "SELECT NW.word, WF.frequency, SD.DATE, SD.SOURCE " +
+      "FROM WORD_FREQUENCY WF " +
+      "JOIN NEWS_WORD NW on WF.NEWS_WORD_ID = NW.ID " +
+      "JOIN SOURCE_DATE SD on WF.SOURCE_DATE_ID = SD.ID " +
+      "WHERE NW.word = '" + args(1).toLowerCase + "' " +
+      "AND SD.DATE BETWEEN '" + args(2) + "' AND '" + args(3) + "' " +
+      "ORDER BY SD.DATE;"
     val frequencyPerSourceDF = spark.sql(frequencyPerSourceQuery)
     frequencyPerSourceDF.show(frequencyPerSourceDF.count.toInt)
 
+    val frequencyPerDayQuery = "SELECT NW.word, SUM(WF.frequency), SD.DATE " +
+      "FROM WORD_FREQUENCY WF " +
+      "JOIN NEWS_WORD NW on WF.NEWS_WORD_ID = NW.ID " +
+      "JOIN SOURCE_DATE SD on WF.SOURCE_DATE_ID = SD.ID " +
+      "WHERE NW.word = '" + args(1).toLowerCase + "' " +
+      "AND SD.DATE BETWEEN '" + args(2) + "' AND '" + args(3) + "' " +
+      "GROUP BY SD.DATE, NW.word " +
+      "ORDER BY SD.DATE;"
     val frequencyPerDayDF = spark.sql(frequencyPerDayQuery)
     frequencyPerDayDF.show(frequencyPerDayDF.count.toInt)
 
     spark.stop()
   }
 }
-//TODO logs schreiben
-//TODO in Klasse auslagern
+//TODO include logs
+//TODO write tests
