18435d14765ff1e703a343159662627023f073ea
Max Manthey <mantheymax23@gmail.com>
2022-09-25 23:52:38 +0200

    changed parquet persistence

--- src/main/scala/parquet/queries/ParquetPersistenceHelper.scala
+++ src/main/scala/parquet/queries/ParquetPersistenceHelper.scala
@@ -3,15 +3,15 @@ package parquet.queries
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 
-case class ParquetPersistenceHelper(spark: SparkSession, parquetPath: String) {
-  def createParquetFile(articleDF: DataFrame): Unit =
+case class ParquetPersistenceHelper(spark: SparkSession) {
+  def createParquetFile(articleDF: DataFrame, parquetPath: String): Unit =
     articleDF.write.mode(SaveMode.Overwrite).parquet(parquetPath)
 
 
-  def appendToParquetFile(articleDF: DataFrame): Unit =
+  def appendToParquetFile(articleDF: DataFrame, parquetPath: String): Unit =
   articleDF.write.mode(SaveMode.Append).parquet(parquetPath)
 
 
-  def readParquetFileAsDF(): DataFrame =
+  def readParquetFileAsDF(parquetPath: String): DataFrame =
     spark.read.parquet(parquetPath)
 }
