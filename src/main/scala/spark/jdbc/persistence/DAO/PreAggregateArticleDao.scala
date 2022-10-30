b9263087c55a850b01ea653d6c0ca7b5dbc84caf
Max Manthey <mantheymax23@gmail.com>
2022-10-30 19:39:35 +0100

    changed aggregation impl

--- src/main/scala/spark/jdbc/persistence/DAO/PreAggregateArticleDao.scala
+++ src/main/scala/spark/jdbc/persistence/DAO/PreAggregateArticleDao.scala
@@ -19,26 +19,19 @@ case class PreAggregateArticleDao(spark: SparkSession, connectionUrl: String) {
       val sourceDateDF = jdbcConnection.getTableAsDataframe("source_date", spark)
       val wordFrequencyDF = jdbcConnection.getTableAsDataframe("word_frequency", spark)
 
-      val distinctWords = newsWordDF.collect()
+      val aggregatedWordFrequencyDF = wordFrequencyDF.as("wf")
+        .join(newsWordDF.as("nw"), wordFrequencyDF("news_word_id") ===  newsWordDF("id"),"full")
+        .join(sourceDateDF.as("sd"), wordFrequencyDF("source_date_id") === sourceDateDF("id"), "full")
+        .select("nw.id", "wf.frequency", "sd.date")
+        .groupBy(newsWordDF("id").as("news_word_id"), sourceDateDF("date"))
+        .agg(sum("wf.frequency"))
+        .withColumnRenamed("sum(wf.frequency)", "frequency")
+        .orderBy(sourceDateDF("date"))
 
-      for(distinctWord <- distinctWords) {
-        val word = distinctWord(1)
-
-        val aggregatedWordFrequencyDF = wordFrequencyDF.as("wf")
-          .join(newsWordDF.as("nw"), wordFrequencyDF("news_word_id") ===  newsWordDF("id"),"full")
-          .join(sourceDateDF.as("sd"), wordFrequencyDF("source_date_id") === sourceDateDF("id"), "full")
-          .where(newsWordDF("word") === word)
-          .select("nw.id", "wf.frequency", "sd.date")
-          .groupBy(newsWordDF("id").as("news_word_id"), sourceDateDF("date"))
-          .agg(sum("wf.frequency"))
-          .withColumnRenamed("sum(wf.frequency)", "frequency")
-          .orderBy(sourceDateDF("date"))
-
-        aggregatedWordFrequencyDF
-          .write
-          .mode(SaveMode.Append)
-          .jdbc(connectionUrl, "AGGREGATED_WORD_FREQUENCY", connectionProperties)
-      }
+      aggregatedWordFrequencyDF
+        .write
+        .mode(SaveMode.Append)
+        .jdbc(connectionUrl, "AGGREGATED_WORD_FREQUENCY", connectionProperties)
     } catch {
       case e: Exception => println(s"Error trying to add preAggregateArticle ${e.getCause}")
         e.printStackTrace()
